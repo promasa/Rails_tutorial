86dff53b6d3c9c5a10b4605f737f553e3bc7e227
MasahiroWatanabe <watanabesusumuhiroshi@watanabemasahiro-MacBook-Pro.local>
2020-01-21 13:56:56 +0900

    Chapter 8 End

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   resources :users
   get    '/login',   to: 'sessions#new'
-  post   '/login/post',   to: 'sessions#create'
+  post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 end
